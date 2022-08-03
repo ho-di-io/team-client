39dbbf86b01d3cad43b67a2c2b190c1aa9daa908
devfrank9 <devfrank9@gmail.com>
2022-08-03 22:42:13 +0900

    Feat: "Logo 초안 중간저장"

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,3 +1,24 @@
+import styled from 'styled-components';
+import images from '@images/index';
+
 export default function Main() {
-  return <></>;
+  return (
+    <Container>
+      <Logo>
+        <img src={images.Logo} alt="" />
+        <img src={images.LogoText} alt="" />
+      </Logo>
+      <Tap>
+        <span>Team</span>
+        <span>Projects</span>
+      </Tap>
+    </Container>
+  );
 }
+
+export const Container = styled.div`
+  width: 100%;
+  height: 10rem;
+`;
+export const Logo = styled.div``;
+export const Tap = styled.div``;
